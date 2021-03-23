a06ac31a93b0e54d5ba29067db73f494ff323993
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-03-23 15:20:25 +0530

    initial

---
+++ my-next-netlifycms/pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+// import styles from '../styles/Home.module.css';
+import { attributes, react as HomeContent } from '../content/home.md';
+
+export default function Home() {
+  let { title, cats } = attributes;
+  return (
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <article>
+        <h1>{title}</h1>
+        <HomeContent />
+        <ul>
+          {cats.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </>
+  );
+}
